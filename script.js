a517388351a1a813870ab3820b05ba0636206e28
Maheshbilla03 <maheshbilla03@gmail.com>
2026-05-07 22:34:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+
+  const icon = hamburger.querySelector("i");
+
+  if (navLinks.classList.contains("active")) {
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-xmark");
+  } else {
+    icon.classList.remove("fa-xmark");
+    icon.classList.add("fa-bars");
+  }
+});
